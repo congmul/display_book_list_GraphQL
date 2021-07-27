9b2a9378cedb13e73adc82ebf56dfdca6b634ac2
Jehyun <congmul@hotmail.com>
2021-07-27 09:56:26 -0700

    Update for Preventing Null input

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -9,7 +9,8 @@ const {
     GraphQLSchema,
     GraphQLID,
     GraphQLInt,
-    GraphQLList
+    GraphQLList,
+    GraphQLNonNull
 } = graphql;
 
 // // dummy data
@@ -39,7 +40,7 @@ const BookType = new GraphQLObjectType({
             resolve(parent, args){
                 // console.log(parent);
                 // return _.find(authors, { id: parent.authorId })
-
+                return Author.findById(parent.authorId);
             }
         }
     })
@@ -55,6 +56,7 @@ const AuthorType = new GraphQLObjectType({
             type: new GraphQLList(BookType), // Author has Books list
             resolve(parent, args){
                 // return _.filter(books, { authorId: parent.id })
+                return Book.find({ authorId: parent.id});
             }
         }
     })
@@ -70,6 +72,7 @@ const RootQuery = new GraphQLObjectType({
                 // code to get data from db / other source
                 // console.log(typeof args.id);
                 // return _.find(books, { id: args.id });
+                return Book.findById(args.id)
             }
         },
         author: {
@@ -77,18 +80,21 @@ const RootQuery = new GraphQLObjectType({
             args: {id:{ type: GraphQLID }},
             resolve(parent, args){
                 // return _.find(authors, { id: args.id });
+                return Author.findById(args.id)
             }
         },
         books: {
             type: new GraphQLList(BookType),
             resolve(parent, args){
                 // return books
+                return Book.find({})
             }
         },
         authors: {
             type: new GraphQLList(AuthorType),
             resolve(parent, args){
                 // return authors
+                return Author.find({})
             }
         }
     }
@@ -100,8 +106,8 @@ const Mutation = new GraphQLObjectType({
         addAuthor: {
             type: AuthorType,
             args: {
-                name: { type: GraphQLString },
-                age: { type: GraphQLInt }
+                name: { type: new GraphQLNonNull(GraphQLString)  },
+                age: { type: new GraphQLNonNull(GraphQLInt) }
             },
             resolve(parent, args){
                 let author = new Author({   // Create Author Model instance
@@ -115,9 +121,9 @@ const Mutation = new GraphQLObjectType({
         addBook: {
             type: BookType,
             args: {
-                name: { type: GraphQLString },
-                genre: { type: GraphQLString },
-                authorId: { type: GraphQLID },
+                name: { type: new GraphQLNonNull(GraphQLString) }, // Prevent Null for the argument
+                genre: { type: new GraphQLNonNull(GraphQLString) },
+                authorId: { type: new GraphQLNonNull(GraphQLID)  },
             },
             resolve(parent, args){
                 let book = new Book({   // Create Author Model instance
