0f69ee99618f0a6cc8efe3112d36b5fdda51f3d0
Jehyun <congmul@hotmail.com>
2021-07-27 09:42:25 -0700

    Add Mongoose and connect it with graphQL

--- server/app.js
+++ server/app.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require("mongoose");
+
 
 const app = express();
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/graphQLPractice", { useNewUrlParser: true });
+mongoose.connection.once('open', () => {
+    console.log('conneted to database')
+})
 
 app.use('/graphql', graphqlHTTP({
     // pass Schema
